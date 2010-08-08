6da6da2dd4f73cce1d57737991f4888fa22ee302
pycmam <pycmam@gmail.com>
2010-08-08 07:44:31 +0600

    cc_id в форме

--- lib/form/doctrine/PluginmyQuestionForm.class.php
+++ lib/form/doctrine/PluginmyQuestionForm.class.php
@@ -14,10 +14,12 @@ abstract class PluginmyQuestionForm extends BasemyQuestionForm
     {
         parent::setup();
 
+        $this->widgetSchema['cc_id'] = new sfWidgetFormInputHidden();
+
         $this->validatorSchema['email'] = new sfValidatorEmail();
         $this->validatorSchema['question']->setOption('min_length', 5);
         $this->validatorSchema['question']->setOption('max_length', 1024);
 
-        $this->useFields(array('name', 'email', 'phone', 'subj', 'question'));
+        $this->useFields(array('name', 'email', 'phone', 'subj', 'question', 'cc_id'));
     }
 }
