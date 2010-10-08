c2cb66804a9c285d42be18b21801c7ec08d3d55f
pycmam <pycmam@gmail.com>
2010-10-08 21:21:46 +0600

    Codestyle

--- modules/myQuestion/templates/_form.php
+++ modules/myQuestion/templates/_form.php
@@ -1,8 +1,10 @@
 <?php use_helper('jQuery') ?>
 
-<?php echo jq_form_remote_tag(array('url' => '@myQuestion', 'update' => 'question-form'), array('class' => 'list question-form')) ?>
-    <ul>
-    <?php echo $form ?>
-    <li><input id="question-submit" type="submit" value="<?php echo __('Submit') ?>" /></li>
+<?php echo jq_form_remote_tag(array('url' => 'myQuestion', 'update' => 'question-form'), array('class' => 'question-form')) ?>
+    <ul class="form">
+        <?php echo $form ?>
+        <li class="form-row">
+            <input id="question-submit" type="submit" value="<?php echo __('Submit') ?>" />
+        </li>
     </ul>
 </form>
