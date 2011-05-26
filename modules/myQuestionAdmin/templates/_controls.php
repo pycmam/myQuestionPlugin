0b1eb8cbe0cb78e3187fd69e10f97b7bea69d981
pycmam <pycmam@gmail.com>
2011-05-26 11:19:48 +0600

    Убрал ссылку "Удалить" из просмотра вопроса

--- modules/myQuestionAdmin/templates/_controls.php
+++ modules/myQuestionAdmin/templates/_controls.php
@@ -9,6 +9,5 @@
 <div id="object_controls">
 <ul>
     <li class="back-to-list section"><?php echo link_to('Вернуться в список', 'myQuestionAdmin'); ?></li>
-    <li class="delete section"><?php echo link_to('Удалить', 'myQuestionAdmin_delete', $my_question, array('method' => 'delete', 'confirm' => 'Удалить вопрос?')); ?></li>
 </ul>
 </div>
