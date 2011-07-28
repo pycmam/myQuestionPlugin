a50472becbf3117df919a3d9a8701cb79410b352
pycmam <pycmam@gmail.com>
2011-07-28 15:56:58 +0600

    Form/QuestionForm: email validator

--- lib/form/doctrine/PluginmyQuestionForm.class.php
+++ lib/form/doctrine/PluginmyQuestionForm.class.php
@@ -17,7 +17,7 @@ abstract class PluginmyQuestionForm extends BasemyQuestionForm
         $this->widgetSchema['cc_id'] = new sfWidgetFormInputHidden();
 
         $this->validatorSchema['phone'] = new sfValidatorString(array('max_length' => 255, 'trim' => true));
-        $this->validatorSchema['email'] = new sfValidatorEmail();
+        $this->validatorSchema['email'] = new myValidatorEmail();
         $this->validatorSchema['question']->setOption('min_length', 5);
         $this->validatorSchema['question']->setOption('max_length', 1024);
 
