1c4434c59df899f114f7a6849bffcc4e314fc19a
pycmam <pycmam@gmail.com>
2011-10-28 07:07:17 +0600

    bugfix

--- modules/myQuestionAdmin/templates/showSuccess.php
+++ modules/myQuestionAdmin/templates/showSuccess.php
@@ -46,10 +46,6 @@
                     <strong class="label">Телефон:</strong>
                     <p class="content"><?php echo $question->getPhone() ?></p>
                 </div>
-                <div class="row">
-                    <strong class="label">Город:</strong>
-                    <p class="content"><?php echo $question->getCity() ?></p>
-                </div>
             </div>
         </div>
 
